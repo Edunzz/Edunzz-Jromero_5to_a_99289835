b4593c46369e5183fd34a9faed639209af89368a
JoseRomero616 <jo.seeduardo25397stk@gmail.com>
2025-03-05 19:18:20 -0500

    mi primer push a git

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// js/script.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Aquí puedes agregar tus scripts personalizados
+    console.log("Página cargada correctamente");
+  });
+  
